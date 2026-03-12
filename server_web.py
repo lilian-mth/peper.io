bc7777af18aac055ce52df8918f50fe6afd1bfac
lilian-mth <lilianmathieu14@gmail.com>
2026-03-12 19:17:45 +0100

    moves v5

--- server_web.py
+++ server_web.py
@@ -9,7 +9,7 @@ import time
 # --- 1. CONFIGURATION ET ÉTAT DU JEU ---
 HOST = '0.0.0.0'
 PORT = int(os.environ.get("PORT", 5555))
-TICK_RATE = 0.250
+TICK_RATE = 0.050
 N = 50 # <-- LA CARTE EST AGRANDIE ICI AUSSI !
 
 PALETTE_COULEURS = [
